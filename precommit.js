b1a4d2026ffb7f1e54f4c19dd9f74f21b00b127a
Marko <markodrakulic@Markos-MacBook-Air-2.local>
2024-03-15 13:00:33 +0100

    feature: Use viem to check dirnames

--- precommit.js
+++ precommit.js
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import { isAddress } from "viem";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -21,7 +22,9 @@ fs.readdir(logosDir, (err, files) => {
       }
 
       if (stats.isDirectory()) {
-        console.log(`Directory: ${file}`);
+        if (!isAddress(file)) {
+          throw new Error(`File ${file} is not a valid address!`);
+        }
       }
     });
   });
